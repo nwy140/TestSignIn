12d3c90920db372d18e1804ebefa84ef67381500
SInt-Win11 <throwawayspectacle@gmail.com>
2023-02-15 10:00:25 +0800

    a

---
+++ r.js
@@ -0,0 +1,16 @@
+
+
+
+
+  function downloadObjectAsJson(exportObj, exportName){
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
+
+
+// https://stackoverflow.com/questions/19721439/download-json-object-as-a-file-from-browser/30800715#30800715
